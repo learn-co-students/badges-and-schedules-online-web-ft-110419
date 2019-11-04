13dd23305b07c9ab6eb4e52aa3deca947254f400
Jess Fischbach <robotspacefish@gmail.com>
2019-11-04 22:08:13 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
+require 'pry'
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each{ |name| badges << badge_maker(name) }
+  badges
+end
+
+def assign_rooms(speakers)
+  list = []
+  speakers.each_with_index do |speaker, room|
+    list << "Hello, #{speaker}! You'll be assigned to room #{room+1}!"
+  end
+
+  list
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|attendee| puts attendee}
+  assign_rooms(attendees).each {|name| puts name}
+end
