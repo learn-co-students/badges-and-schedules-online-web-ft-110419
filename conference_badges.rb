19bb69f681a34e82eed6b300af7b1835202a058a
Ingrid sura <sura.ingrid@gmail.com>
2019-11-11 03:11:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+name=""
+def badge_maker("name") 
+  puts "Hello, my name is #{|name|}."
+  puts badge_maker("name")
+end
+
+attendies=[Edsger, Ada, Charles, Alan, Grace, Linus, Matz]
+batch_badge_creator each.do(|#{attendies}|)
+  
